5282cf50321f1b16bd6b43c2472718d02cd9bb49
Strelnikova Vasilisa <vasilina_18@mail.ru>
2023-01-16 01:26:48 +0700

    initial

--- tests/test_buy_cosmetics.py
+++ tests/test_buy_cosmetics.py
@@ -28,43 +28,44 @@ def test_buy_product():
     driver.get(base_url)
     driver.maximize_window()
     mp = Main_page(driver)
-    mp.goto_for_face()
+    mp.goto_for_face() #переход на страницу товаров для лица
     ffp = Forface_page(driver)
-    ffp.put_mask()
-    ffp.goto_hair()
+    ffp.put_mask() # добавляем маску в корзину
+    ffp.goto_hair() #переходим на страницу товаров для волос
     ffh = Forhair_page(driver)
-    ffh.shampoo_to_cart()
-    ffh.goto_description_broccoli_balm()
+    ffh.shampoo_to_cart() # добавляем шампунь в корзину
+    ffh.goto_description_broccoli_balm() # переходим в описание бальзама
     bbp = Broccolibalm_page(driver)
-    bbp.put_balm()
-    bbp.goto_cart()
+    bbp.put_balm() # добавляем бальзам в корзину
+    bbp.goto_cart() # переходим в корзину
     cp = Cart_page(driver)
-    price_shampoo_1 = cp.take_price(cp.get_shampoo_price())
-    cp.plus_shampoo()
+    price_shampoo_1 = cp.take_price(cp.get_shampoo_price()) #цена шампуня до изменений
+    cp.plus_shampoo() # добавляем еще один шампунт
     time.sleep(2)
-    cp.minus_shampoo()
+    cp.minus_shampoo() # отнимаем шампунь
     time.sleep(2)
-    cp.plus_shampoo()
+    cp.plus_shampoo() #снова прибавляем шампунь
     time.sleep(3)
-    price_shampoo_2 = cp.take_price(cp.get_shampoo_price())
+    price_shampoo_2 = cp.take_price(cp.get_shampoo_price()) # цена шампуней
     assert price_shampoo_1 < price_shampoo_2, "Не сработали кнопки +/- на продукте"
 
-    cp.input_quantity_balm(3)
+    cp.input_quantity_balm(3) # вводим кол-во бальзамов с клавиатуры
     time.sleep(2)
-    cp.delete_mask()
-    try:
+    cp.delete_mask() # удаляем маску
+    try: # проверяем, что маска удалилась
         cp.delete_mask()
     except TimeoutException:
         print("Продукт успешно удален")
     else:
         raise MyError("Продукт не был удален")
 
-    try:
+    try: #проверяем, что стоимость товаров в корзине соответствует посчитанной итоговой стоимости
         cp.check_price(cp.get_total_price(), cp.get_balms_price(), cp.get_shampoo_price())
     except AssertionError:
         print("Стоимость не совпадает!!!")
 
     cp.checkout()
+    """Ввод данных пользователя"""
     chp = Checkout_page(driver)
     chp.do_input_phone()
     chp.choose_post()
